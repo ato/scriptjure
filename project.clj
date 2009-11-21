fd0803dd8f1ad5843567e3913c2e825db2fe36f3
Alex Osborne <ato@meshy.org>
2009-11-21 21:46:24 +1100

    Make buildable with Leiningen.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject org.clojars.ato/scriptjure "0.1.0-SNAPSHOT"
+  :description "Generates javascript from Clojure forms."
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
